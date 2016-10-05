6e4a848277f7c6cbdee04e55577a6c5dcf8fb90b
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-06 01:15:08 +0300

    NEW: TS support

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,39 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-    devtool: 'source-map',
-    entry: [
-        './src/app.js'
-    ],
+    entry: ['./src/app.tsx'],
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
         publicPath: '/static/'
     },
+
     module: {
-        loaders: [
-            {
-                test: /\.js?/,
-                loaders: ['babel'],
-                include: path.join(__dirname, 'src')
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
-            }
-        ]
-    }
-}
+        loaders: [{
+            test: /\.js?/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.scss$/,
+            loaders: ['style', 'css', 'sass']
+        }, {
+            test: /\.tsx?$/,
+            loader: "ts-loader"
+        }],
+
+        preLoaders: [{
+            test: /\.js$/,
+            loader: "source-map-loader"
+        }]
+    },
+};
