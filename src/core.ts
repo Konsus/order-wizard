fb7dccb9d83eef9d59d4c7b0abe64dd7faa2ebf5
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-08 07:11:11 +0300

    UPDATE: interfaces

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,32 @@
 module Survey {
+
+    type Function<T1, T2, R> = (x: T1, y: T2) => R;
+    type Function<T, R> = (x: T) => R;
+    type Function<R> = () => R;
+
+    export interface Questionnaire {
+        pages: QuestionsPage[];
+    }
+
+    export interface QuestionsPage {
+        title?: string;
+        questions: Question | Question[];
+    }
+
+    export interface Question {
+        token?: string | number;
+        options?: Option[];
+        title?: string;
+        required?: boolean;
+        defaultValue?: any;
+    }
+
+    export interface Option {
+        value: any;
+        label?: string;
+        //defaultValue?: boolean,
+    }
+
     export type Ref<T> = (instance: T) => any;
 
     export interface Value {
@@ -13,10 +41,10 @@ module Survey {
     export interface Field {
         value?: any;
         token: React.Key;
-        form: SurveyForm;
+        //form: SurveyForm;
     }
 
-    export interface Checkbox extends Element {
+    export interface CheckBox extends Element {
         defaultChecked?: boolean;
     }
 
@@ -29,15 +57,42 @@ module Survey {
         value?: any;
     }
 
-    export interface Question extends Group {
-        token: React.Key;
-        form: SurveyForm;
-        title?: string;
-        required: boolean;
-    }
-
     export interface SurveyForm {
         getFormValue(key: React.Key): any;
         setFormValue(key: React.Key, value: any);
     }
+
+    type QuestionFn<R> = (q: Question) => R;
+    type QuestionFn<F, R> = (q: Question, form: F) => R;
+
+    //export interface QuestionState {
+    //    active?: QuestionFn<boolean>;
+    //}
+
+    export interface QuestionState<F> {
+        active?: QuestionFn<F, boolean> | QuestionFn<boolean>;
+    }
+}
+
+module Survey.View {
+
+    export interface Value {
+        value?: any;
+    }
+
+    export type Ref<T> = (instance: T) => any;
+
+    export interface Element extends Value {
+        label: string;
+    }
+
+    export interface CheckBox extends Element {
+        group: Group;
+        defaultChecked?: boolean;
+    }
+
+    export interface Group {
+        checked(value: any): boolean;
+    }
+
 }
