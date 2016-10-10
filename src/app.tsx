5edee25ddea2ffdfa14cad8df02d1fb7e897afd8
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-10 05:09:47 +0300

    FIX: app now display 2 buttons

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import {render} from "react-dom";
 import {SurveyWindow} from "./components/views/SurveyWindow";
 import {PowerPointProject} from "./surveys/PowerPointProject";
+import {DataEntryProject} from "./surveys/DataEntryProject";
 
 class Container extends React.Component<any,any> {
     render(): JSX.Element|any {
-        return <SurveyWindow>
-            <PowerPointProject/>
-        </SurveyWindow>
+        return (
+            <div>
+                <SurveyWindow name={"PowerPoint"} visible={true}> <PowerPointProject/> </SurveyWindow>
+                <SurveyWindow name={"DataEntry"}> <DataEntryProject/> </SurveyWindow>
+            </div>
+        );
     }
 }
 
