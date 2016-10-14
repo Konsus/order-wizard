4b1e93dff6092093e66cd8e890851553925ff9a5
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-14 03:03:30 +0300

    NEW: writing project button in app

--- app/app.tsx
+++ app/app.tsx
@@ -3,13 +3,15 @@ import {render} from "react-dom";
 import {SurveyWindow} from "../src/components/views/SurveyWindow";
 import {PowerPointProject} from "../src/surveys/PowerPointProject";
 import {DataEntryProject} from "../src/surveys/DataEntryProject";
+import {WritingProject} from "../src/surveys/WritingProject";
 
 class Container extends React.Component<any,any> {
     render(): JSX.Element|any {
         return (
             <div>
-                <SurveyWindow name={"PowerPoint"} visible={true}> <PowerPointProject/> </SurveyWindow>
+                <SurveyWindow name={"PowerPoint"}> <PowerPointProject/> </SurveyWindow>
                 <SurveyWindow name={"DataEntry"}> <DataEntryProject/> </SurveyWindow>
+                <SurveyWindow name={"Writing"}> <WritingProject/> </SurveyWindow>
             </div>
         );
     }
