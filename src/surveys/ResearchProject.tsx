8096a7d296985b964da78ab8d12c56e8d99b4fe0
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-14 03:17:46 +0300

    NEW: ResearchProject view

---
+++ src/surveys/ResearchProject.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import {autobind} from "core-decorators";
+import {SurveyFlow} from "../core/survey-flow";
+import {Comment} from "../components/controls/Comment";
+import {File} from "../components/controls/File";
+import {RadioGroup} from "../components/controls/RadioGroup";
+import {SurveyPage} from "../components/views/SurveyPage";
+import {ProjectSurveyFlow} from "../components/views/ProjectSurveyFlow";
+import {DueDatePageView} from "./views/DueDatePageView";
+import Q from "../data/projects/research-project";
+
+export class ResearchProject extends ProjectSurveyFlow<Survey.Forms.ResearchProject> {
+
+    constructor() { super(Q.survey); }
+
+    protected initFlow(flow: SurveyFlow) {
+        flow.setPageView(Q.service, {render: this.renderServiceTypePage});
+        flow.setPageView(Q.files, {render: this.renderFilesPage});
+        flow.setPageView(Q.deadline, {render: this.renderDeadlinePage});
+        flow.setPageView(Q.comments, {render: this.renderCommentsPage});
+    }
+
+    @autobind
+    renderServiceTypePage(page: Survey.Page) {
+        return (
+            <SurveyPage {...page} >
+                <RadioGroup {...page.questions[0]} />
+            </SurveyPage>
+        )
+    }
+
+    @autobind
+    renderFilesPage(page: Survey.Page) {
+        return (
+            <SurveyPage {...page} >
+                <File {...page.questions[0]} />
+            </SurveyPage>
+        )
+    }
+
+    @autobind
+    renderDeadlinePage(page: Survey.Page) {
+        return <DueDatePageView {...page} />
+    }
+
+    @autobind
+    renderCommentsPage(page: Survey.Page) {
+        return (
+            <SurveyPage {...page} >
+                <Comment {...page.questions[0]} />
+            </SurveyPage>
+        )
+    }
+
+}
