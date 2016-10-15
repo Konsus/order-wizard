7bf5dc959219d4b5d9bd31ac5a2cda228379052b
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-15 05:15:58 +0300

    FIX: question were unable to check form values since there were in data container

--- src/core/survey-form.ts
+++ src/core/survey-form.ts
@@ -1,22 +1,28 @@
 import {SyncEvent} from "ts-events";
 export class SurveyForm implements Survey.SurveyForm {
 
-    readonly onPropertyChanged: SyncEvent<string> = new SyncEvent<string>();
-    private readonly data = {};
+    private readonly _onPropertyChanged: SyncEvent<string> = new SyncEvent<string>();
+    get onPropertyChanged(): SyncEvent<string> { return this._onPropertyChanged; }
 
     getValue(key: string): any {
-        return this.data[key];
+        return this[key];
     }
 
     setValue(key: string, value: any) {
         console.log("set form value >> " + key + ": " + value);
-        const prev = this.data[key];
+        const prev = this[key];
         if (prev == value) return;
-        this.data[key] = value;
-        this.onPropertyChanged.post(key);
+        this[key] = value;
+        this._onPropertyChanged.post(key);
     }
 
     toJSON(): any {
-        return this.data;
+
+        const json = {};
+        for (var key in this)
+            if (!key.startsWith('_'))
+                json[key] = this[key];
+
+        return json;
     }
 }
