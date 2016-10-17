02e3cae3b126a8dfd5e2a9ea07fe173b2be9012c
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-17 04:44:14 +0300

    UPDATE: build script

--- publish.sh
+++ publish.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-branch=$(git symbolic-ref --short -q HEAD)
-git stash
-git checkout -B release
+# save branch
+branch="$(git symbolic-ref --short -q HEAD)"
+# checkout to release branch
+git checkout release
 git merge origin/master
-npm run lib
-git add --force ./lib
-git commit -m '[auto-release]'
+# compile
+compile="$(npm run lib >/dev/null)"
+if [ -n "$compile" ]; then
+    echo "Compilation has errors!"
+else
+    # publish
+    echo "Publish!"
+    git add --force ./lib
+    git commit -m '[auto-release]'
+fi
+# switch back
+git checkout -- ./lib
 echo "switch back to $branch"
-git checkout $branch
-git stash pop
+git checkout ${branch}
