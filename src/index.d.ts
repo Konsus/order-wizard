706d10c4993a5c47b55ef6f1987eb5b2c9899ae3
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-24 01:54:30 +0300

    FIX: typings for frontapp integration

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./core/index.d.ts" />
+/// <reference path="./data/common/index.d.ts" />
+/// <reference path="./data/projects/index.d.ts" />
+/// <reference path="./surveys/index.d.ts" />
