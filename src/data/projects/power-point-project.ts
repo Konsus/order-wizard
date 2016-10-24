c6cdb569e192c127c4a6b2fb6858c798e1370a36
Max Stankevich <m.stankevicsh@gmail.com>
2016-10-24 23:29:05 +0300

    FIX: power point template question

--- src/data/projects/power-point-project.ts
+++ src/data/projects/power-point-project.ts
@@ -22,12 +22,12 @@ export const TemplateQuestion: Survey.Question = {
     token: nameof((null as PowerPointProject).template),
     title: "Do you have an existing template we should use?",
     options: [
-        {value: "yes", label: "Yes."}, {
-            value: "no",
-            label: "No, but please include that as part of delivery.",
-            active: (form: PowerPointProject) => form.service != "new-presentation",
-        },
-        {value: "embedded", label: "Use the template the presentation is currently in."}
+        {value: "yes", label: "Yes."},
+        {value: "no", label: "No, but please include that as part of delivery."},
+        {
+            value: "embedded", label: "Use the template the presentation is currently in.",
+            active: (form: PowerPointProject) => form.service != "new-presentation"
+        }
     ]
 };
 
